5c4de6d03ea78227a21d4cf0de1f33f3f6d0ec91
miyazaki <ryuta_miyazaki@ot.olympus.co.jp>
2019-12-20 16:17:57 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -1,1 +1,10 @@
-// Wrtie your code here.
+// pre内のタブ文字削除(タブインデントが出来る)
+window.addEventListener("DOMContentLoaded", function () {
+	// preを取得
+	var pres = document.getElementsByTagName("code");
+	// 反復処理
+	for (var i = 0; i < pres.length; i++) {
+		// タブ文字を消す
+		pres[i].firstChild.data = pres[i].firstChild.data.replace(/^\t+/gm, "");
+	}
+}, false);
