ebde760642c98845282b3156c97089a112b7a206
miyazaki <ryuta_miyazaki@ot.olympus.co.jp>
2019-12-27 17:32:35 +0900

    Chart.js追加

--- js/common.js
+++ js/common.js
@@ -17,3 +17,75 @@ document.addEventListener('DOMContentLoaded', function () {
 		direction: 'left'
 	});
 });
+
+/* Chart.js */
+document.addEventListener('DOMContentLoaded', function () {
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			},
+			events: ['click']
+		}
+	});
+
+	var ctx2 = document.getElementById('mixedChart').getContext('2d');
+	var mixedChart = new Chart(ctx2, {
+		type: 'bar',
+		data: {
+			datasets: [{
+				label: 'Bar Dataset',
+				data: [10, 20, 30, 40],
+				// this dataset is drawn below
+				order: 1
+			}, {
+				label: 'Line Dataset',
+				data: [10, 10, 10, 10],
+				type: 'line',
+				// this dataset is drawn on top
+				order: 2
+			}],
+			labels: ['January', 'February', 'March', 'April']
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			},
+			events: ['click']
+		}
+	});
+});
